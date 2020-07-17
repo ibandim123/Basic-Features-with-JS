2026fb3cc903edf1aefd32710def932b560833f4
Isaac Bandim <ibandim@outlook.com>
2020-07-17 08:04:21 -0300

    edit

--- upload.js
+++ upload.js
@@ -3,8 +3,8 @@ const morgan = require("morgan");//Install morgan for requisition log of http
 
 const app = express(); //express call
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.json()); // App read req in json format
+app.use(express.urlencoded({extended: true}))// Express read req in url uncoded.
 app.use(morgan('dev'));
 
 app.use(require("./routes"));
